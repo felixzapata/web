535d81818399fb1acd3a137a1f96d4bfbdd2f5e6
Felix Zapata <fzberlinches@gmail.com>
2017-06-04 10:48:37 +0200

    feat(polymer-2): change folder and sources according the base starter

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,5 @@
-console.info('Service worker disabled for development, will be generated at build time.');
+/* eslint no-console: ["error", { allow: ["info"] }] */
+
+console.info(
+  'Service worker disabled for development, will be generated at build time.'
+);
